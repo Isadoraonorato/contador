03352133a29e84ba2b8fd5ec2c5b6ba234e98174
Isadoraonorato <isa.onorato@gmail.com>
2022-06-12 11:52:50 -0300

    Contador encontro remoto 2

---
+++ contador.js
@@ -0,0 +1,13 @@
+let NumeroDeAlunos = 10
+
+for (let contador = 0; contador <= NumeroDeAlunos; contador++){
+    if (contador == 0){
+        console.log["O numero atrual é zero"]
+    }
+    else if(contador % 2 == 0){
+        console.log["O numero " + contador + " é PAR"]
+    }
+    else{
+        console.log["O nuemro " + contador + "é IMPAR"]
+    }
+}
